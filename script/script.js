a95e3a510dc20f57467018a5b816202cafcd1d03
curlogianvito <162125204+curlogianvito@users.noreply.github.com>
2024-05-13 09:44:24 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,7 +1,25 @@
-function doLogin() {
-  var username = document.getElementById("username").value;
-  var psw = document.getElementById("password").value;
-  if( username == "" || psw == ""){
-    alert("Username e password obbligatori");
-  }
-}
+function doLogin(event) {
+   var username = document.getElementById("username").value;
+   var psw = document.getElementById("password").value;
+   if(username=="" || psw =="") {
+       alert("Username e password sono obbligatori");
+   } else }
+     fetch('https://www.cacciaapi.altervista.org/login.php/loginIpad', { 
+             method: 'POST',
+             headers: { 
+                'Content-Type': 'application/json',
+             }, 
+             body: JSON.stringify({
+                "exampleInputUsername": username, 
+                "exampleInputPassword": password 
+             })
+    }) 
+      .then(response => responsive.json())
+      .then(data => {
+         console.log(data);
+          window.location.href = "game.html"; // Redirect to game.html
+      })
+         .catch((error) =>{
+          console.error('Error:', error);
+      });
+   } 
